89cd54fa26a60b1cb9f74038764a844b0324ec5c
Thiago Rossener <thiago.rossener@gmail.com>
2017-06-13 16:57:53 -0300

    Mocked buy url

--- aloisioimoveis/urls.py
+++ aloisioimoveis/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from aloisioimoveis.core.views import home, rent
+from aloisioimoveis.core.views import home, rent, buy
 
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^alugar/$', rent, name='rent'),
+    url(r'^comprar/$', buy, name='buy'),
     url(r'^admin/', admin.site.urls),
 ]
