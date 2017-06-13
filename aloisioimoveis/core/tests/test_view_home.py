18e9a19e4cb40f998d7820abba8be0d17ecfbfac
Thiago Rossener <thiago.rossener@gmail.com>
2017-06-13 16:37:21 -0300

    Refactoring

--- aloisioimoveis/core/tests/test_view_home.py
+++ aloisioimoveis/core/tests/test_view_home.py
@@ -1,11 +1,15 @@
 from django.test import TestCase
+from django.shortcuts import resolve_url as r
 
 
 class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get(r('home'))
+
     def test_get(self):
-        response = self.client.get('/')
-        self.assertEqual(200, response.status_code)
+        """GET / should return status 200"""
+        self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'index.html')
+        """Must use index.html"""
+        self.assertTemplateUsed(self.response, 'index.html')
--- aloisioimoveis/core/tests/test_view_rent.py
+++ aloisioimoveis/core/tests/test_view_rent.py
@@ -1,13 +1,15 @@
 from django.test import TestCase
+from django.shortcuts import resolve_url as r
 
 
 class RentTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get(r('rent'))
+
     def test_get(self):
         """GET /alugar/ should return status 200"""
-        response = self.client.get('/alugar/')
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
         """Must use rent.html"""
-        response = self.client.get('/alugar/')
-        self.assertTemplateUsed(response, 'rent.html')
+        self.assertTemplateUsed(self.response, 'rent.html')
